e42e3940804ba4b0b8785f6deafe12af1624d659
sabareesh <sabareeshkkanan@gmail.com>
2017-12-24 10:16:56 -0800

    init

---
+++ src/test/java/com/sabareesh/robinhood/RobinhoodApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sabareesh.robinhood;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RobinhoodApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
